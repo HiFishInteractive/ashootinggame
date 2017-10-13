6affc6988f97ceb5123f471e2e203260822a10d2
Zachary Johnson <zacharyjohnson@Zacharys-MacBook-Pro.local>
2017-10-12 22:33:42 -0400

    Built the first, main scene. Placed some place-holder assets using primitives. Starting writing a script (based on a youtube video) for FPS controls. I figure this would keep it simple (compared to the provided FPS controls) and allow us to add only the script we needed.

---
+++ ShootingGameProject/Assets/Scripts/FPScontroller.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPScontroller : MonoBehaviour {
+
+	public float speed = 2f;
+	public float sensitivity = 2f;
+	CharacterController PlayerAvatar;
+
+	public GameObject PlayerCamera;
+
+	float moveFB; //front back
+	float moveLR; //left right
+	float rotX;
+	float rotY;
+
+
+	void Start () {
+		PlayerAvatar = GetComponent <CharacterController> ();
+		
+	}
+	
+
+	void Update () {
+
+		//connect inputs to Axis(x2)
+		moveFB = Input.GetAxis ("Vertical") * speed;
+		moveLR = Input.GetAxis ("Horizontal") * speed;
+
+		rotX = Input.GetAxis ("Mouse X") * sensitivity;
+		rotY = Input.GetAxis ("Mouse Y") * sensitivity;
+		//rotY = Mathf.Clamp (rotY, -60f, 60f);
+
+		Vector3 movement = new Vector3 (moveLR, 0, moveFB);   //Inputs now adjust player vector
+		transform.Rotate (0, rotX, 0);
+		movement = transform.rotation * movement;             //Movement is relative to player rotation
+		PlayerAvatar.Move (movement * Time.deltaTime);
+		PlayerCamera.transform.Rotate (-rotY, 0, 0);
+
+		
+	}
+}
